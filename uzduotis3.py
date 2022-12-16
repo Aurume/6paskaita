a76501543d498f8f5954af3a6e7f7b97ca81a0a3
Aurelija Petrauskaite <aurelija35@gmail.com>
2022-12-16 15:50:51 +0200

    Galutinis variantas.

--- uzduotis3.py
+++ uzduotis3.py
@@ -1,7 +1,21 @@
-# Kompiuterio darbalaukyje (Desktop) sukurtų katalogą „Naujas Katalogas“
-# Šiame kataloge sukurtų tekstinį failą, kuriame būtų šiandienos data ir laikas
-# Atspausdintų šio tekstinio failo sukūrimo datą ir dydį baitais
-# Patarimai:
-#
-# Failo sukūrimo datą galima pasiekti per os.stat(„Failas.txt“).st_ctime
+import os
+import datetime
 
+#os.chdir(r"C:\Users\aurel\Desktop")
+#print(os.getcwd())
+
+# Kompiuterio darbalaukyje (Desktop) sukurtų katalogą „Naujas Katalogas“
+#os.mkdir("Naujas_katalogas")
+
+# Šiame kataloge sukurtų tekstinį failą, kuriame būtų šiandienos data ir laikas
+#os.chdir(r"C:\Users\aurel\Desktop\Naujas_katalogas")
+#print(os.getcwd())
+
+with open("data_ir_laikas.txt", "w") as katalogas:
+    katalogas.write(str(datetime.datetime.today()))
+
+# Atspausdintų šio tekstinio failo sukūrimo datą ir dydį baitais
+# Patarimai: failo sukūrimo datą galima pasiekti per os.stat(„Failas.txt“).st_size
+
+koks_failo_dydis = os.stat("data_ir_laikas.txt").st_size
+print(f"Failo dydis: {koks_failo_dydis}")
