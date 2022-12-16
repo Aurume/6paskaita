d00ed6e234e62cdca3d975b019151fb7aff03e81
Aurelija Petrauskaite <aurelija35@gmail.com>
2022-12-16 16:26:39 +0200

    papildyta

--- uzduotis4.py
+++ uzduotis4.py
@@ -5,3 +5,24 @@
 # Atvaizduotų jau įvestas pajamas ir išlaidas
 # Atvaizduotų įvestų pajamų ir išlaidų balansą (sudėtų visas pajamas ir išlaidas)
 # Patarimas: import pickle
+
+zurnalas = []
+while True:
+    veiksmas = int(input("""1 - įvesti pajamas/išlaidas
+    2 - parodyti pajamas/išlaidas
+    3 - balansas
+    0 - išeiti
+    """))
+    match veiksmas:
+        case 1:
+            suma = float(input("Įveskite sumą: "))
+            zurnalas.append(suma)
+        case 2:
+            print(zurnalas)
+        case 3:
+            print("Balansas yra: ", sum(zurnalas))
+        case 0:
+            print("Viso gero")
+        case _:
+            print("Neteisingas veiksmas")
+
